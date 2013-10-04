Rallaz/LibreCAD

Updated the entity structure

libraries/libdxfrw/src/drw_base.h
@@ -296,6 +296,58 @@ public:
     }
 };
 
+// TNick todo: wrap all elements in this namespace
+namespace libdxfrw	{
+
+//! Special codes for colors
+enum ColorCodes		{
+    ColorByLayer = 256,
+    ColorByBlock = 257
+};
+
+//! Spaces
+enum Space		{
+    ModelSpace = 0,
+    PaperSpace = 1
+};
+
+//! Special kinds of handles
+enum HandleCodes		{
+    NoHandle = -1
+};
+
+//! A group in dxf file
+struct Group {
+    UTF8STRING name;
+    UTF8STRING content;
+};
+
+//! Shadow mode
+enum ShadowMode		{
+    CastAndReceieveShadows = 0,
+    CastShadows = 1,
+    ReceiveShadows = 2,
+    IgnoreShadows = 3
+};
+
+//! Special kinds of materials
+enum MaterialCodes		{
+    MaterialByLayer = 0
+};
+
+//! Special kinds of plot styles
+enum PlotStyleCodes		{
+    DefaultPlotStyle = 0
+};
+
+//! Special kinds of transparencies
+enum TransparencyCodes		{
+    Opaque = 0,
+    Transparent = -1
+};
+
+} // namespace libdxfrw
+
 #endif
 
 // EOF

libraries/libdxfrw/src/drw_entities.h
@@ -16,6 +16,7 @@
 
 #include <string>
 #include <vector>
+#include <list>
 #include "drw_base.h"
 #include "dwgbuffer.h" //RLZ TODO: move type defs to drw_base.h
 
@@ -78,19 +79,26 @@ public:
     //initializes default values
     DRW_Entity() {
         eType = DRW::UNKNOWN;
+        handle = libdxfrw::NoHandle;
         lineType = "BYLAYER";
-        color = 256; // default BYLAYER (256)
+        color = libdxfrw::ColorByLayer; // default BYLAYER (256)
         ltypeScale = 1.0;
         visible = true;
         layer = "0";
         lWeight = DRW_LW_Conv::widthByLayer; // default BYLAYER  (dxf -1, dwg 29)
-        handleBlock = space = 0; // default ModelSpace (0) & handleBlock = no handle (0)
+        handleBlock = 0; //handleBlock = no handle (0)
+        space = libdxfrw::ModelSpace; // default ModelSpace (0) 
         haveExtrusion = false;
         color24 = -1; //default -1 not set
+        shadow = libdxfrw::CastAndReceieveShadows;
+        material = libdxfrw::MaterialByLayer;
+        plotStyle = libdxfrw::DefaultPlotStyle;
+        transparency = libdxfrw::TransparencyCodes;
     }
     virtual~DRW_Entity() {}
 
     DRW_Entity(const DRW_Entity& d) {
+		/* TNick todo: why do we need an explicit copy constructor? */
         eType = d.eType;
         handle = d.handle;
         handleBlock = d.handleBlock;
@@ -104,6 +112,16 @@ public:
         lWeight = d.lWeight;
         space = d.space;
         haveExtrusion = d.haveExtrusion;
+        image = d.image;
+        groups = d.groups;
+        shadow = d.shadow;
+        material = d.material;
+        plotStyle = d.plotStyle;
+        transparency = d.transparency;
+        lTypeH = d.lTypeH;
+        layerH = d.layerH;
+        extAxisX = d.extAxisX;
+        extAxisY = d.extAxisY;
     }
 
     virtual void applyExtrusion() = 0;
@@ -118,16 +136,23 @@ public:
     enum DRW::ETYPE eType;     /*!< enum: entity type, code 0 */
     int handle;                /*!< entity identifier, code 5 */
     int handleBlock;           /*!< Soft-pointer ID/handle to owner BLOCK_RECORD object, code 330 */
-    UTF8STRING layer;              /*!< layer name, code 8 */
-    UTF8STRING lineType;           /*!< line type, code 6 */
+    UTF8STRING layer;          /*!< layer name, code 8 */
+    UTF8STRING lineType;       /*!< line type, code 6 */
     int color;                 /*!< entity color, code 62 */
     enum DRW_LW_Conv::lineWidth lWeight; /*!< entity lineweight, code 370 */
     double ltypeScale;         /*!< linetype scale, code 48 */
     bool visible;              /*!< entity visibility, code 60 */
     int color24;               /*!< 24-bit color, code 420 */
     string colorName;          /*!< color name, code 430 */
-    int space;                 /*!< space indicator 0 = model, 1 paper, code 67*/
+    libdxfrw::Space space;     /*!< space indicator, code 67*/
     bool haveExtrusion;        /*!< set to true if the entity have extrusion*/
+    UTF8STRING image;          /*!< proxy entity graphics, code 92 and 310 */
+    std::list<libdxfrw::Group*> groups; /*!< list of groups, code 102 */
+    libdxfrw::ShadowMode shadow; /*!< shadow mode, code 284 */
+    int material;              /*!< hard pointer id to material object, code 347 */
+    int plotStyle;             /*!< hard pointer id to plot style object, code 420 */
+    int transparency;          /*!< transparency, code 440 */
+    
 //***** dwg parse ********/
     duint8 nextLinkers; //aka nolinks //B
     duint8 plotFlags; //presence of plot style //BB
@@ -164,6 +189,8 @@ public:
     DRW_Coord basePoint;      /*!<  base point, code 10, 20 & 30 */
     double thickness;         /*!< thickness, code 39 */
     DRW_Coord extPoint;       /*!<  Dir extrusion normal vector, code 210, 220 & 230 */
+    // TNick: we're not handling code 50 - Angle of the X axis for 
+    // the UCS in effect when the point was drawn
 };
 
 //! Class to handle line entity

codelibs/libdxfrw

src/drw_base.h
@@ -63,6 +63,7 @@ typedef long double ddouble80;          /* 80 bit floating point */
 
 
 namespace DRW {
+
 //! Version numbers for the DXF Format.
 enum Version {
     UNKNOWNV,     /*!< UNKNOWN VERSION. */
@@ -89,7 +90,54 @@ BAD_READ_TABLES,      /*!< error in tables read process. */
 BAD_READ_ENTITIES     /*!< error in entities read process. */
 };
 
-}
+//! Special codes for colors
+enum ColorCodes {
+    ColorByLayer = 256,
+    ColorByBlock = 0
+};
+
+//! Spaces
+enum Space {
+    ModelSpace = 0,
+    PaperSpace = 1
+};
+
+//! Special kinds of handles
+enum HandleCodes {
+    NoHandle = -1
+};
+
+//! A group in dxf file
+struct Group {
+    std::string name;
+    std::string content;
+};
+
+//! Shadow mode
+enum ShadowMode {
+    CastAndReceieveShadows = 0,
+    CastShadows = 1,
+    ReceiveShadows = 2,
+    IgnoreShadows = 3
+};
+
+//! Special kinds of materials
+enum MaterialCodes {
+    MaterialByLayer = 0
+};
+
+//! Special kinds of plot styles
+enum PlotStyleCodes {
+    DefaultPlotStyle = 0
+};
+
+//! Special kinds of transparencies
+enum TransparencyCodes {
+    Opaque = 0,
+    Transparent = -1
+};
+
+} // namespace DRW
 
 //! Class to handle 3D coordinate point
 /*!

src/drw_entities.h
@@ -16,6 +16,7 @@
 
 #include <string>
 #include <vector>
+#include <list>
 #include "drw_base.h"
 
 class dxfReader;
@@ -94,38 +95,35 @@ public:
     //initializes default values
     DRW_Entity() {
         eType = DRW::UNKNOWN;
+        handle = DRW::NoHandle;
         lineType = "BYLAYER";
-        color = 256; // default BYLAYER (256)
+        color = DRW::ColorByLayer; // default BYLAYER (256)
         ltypeScale = 1.0;
         visible = true;
         layer = "0";
         lWeight = DRW_LW_Conv::widthByLayer; // default BYLAYER  (dxf -1, dwg 29)
-        handleBlock = space = 0; // default ModelSpace (0) & handleBlock = no handle (0)
+        handleBlock = 0; //handleBlock = no handle (0)
+        space = DRW::ModelSpace; // default ModelSpace (0)
         haveExtrusion = false;
         color24 = -1; //default -1 not set
+        shadow = DRW::CastAndReceieveShadows;
+        material = DRW::MaterialByLayer;
+        plotStyle = DRW::DefaultPlotStyle;
+        transparency = DRW::Opaque;
+        inGroup = false;
     }
     virtual~DRW_Entity() {}
-
-    DRW_Entity(const DRW_Entity& d) {
-        eType = d.eType;
-        handle = d.handle;
-        handleBlock = d.handleBlock;
-        layer = d.layer;
-        lineType = d.lineType;
-        color = d.color;
-        color24 = d.color24;
-        colorName = d.colorName;
-        ltypeScale = d.ltypeScale;
-        visible = d.visible;
-        lWeight = d.lWeight;
-        space = d.space;
-        haveExtrusion = d.haveExtrusion;
-    }
 
     virtual void applyExtrusion() = 0;
     virtual bool parseDwg(DRW::Version version, dwgBuffer *buf);
 protected:
-    void parseCode(int code, dxfReader *reader);
+    //! takes an action based on the provided code
+    /*!
+     * \param code the code to interpret
+     * \param reader the reader to use (is asserted to be non-null)
+     * \return true if the code was parsed, false otherwise
+     */
+    bool parseCode(int code, dxfReader *reader);
     void calculateAxis(DRW_Coord extPoint);
     void extrudePoint(DRW_Coord extPoint, DRW_Coord *point);
     bool parseDwgEntHandle(DRW::Version version, dwgBuffer *buf);
@@ -134,16 +132,24 @@ public:
     enum DRW::ETYPE eType;     /*!< enum: entity type, code 0 */
     int handle;                /*!< entity identifier, code 5 */
     int handleBlock;           /*!< Soft-pointer ID/handle to owner BLOCK_RECORD object, code 330 */
-    UTF8STRING layer;              /*!< layer name, code 8 */
-    UTF8STRING lineType;           /*!< line type, code 6 */
+    UTF8STRING layer;          /*!< layer name, code 8 */
+    UTF8STRING lineType;       /*!< line type, code 6 */
     int color;                 /*!< entity color, code 62 */
     enum DRW_LW_Conv::lineWidth lWeight; /*!< entity lineweight, code 370 */
     double ltypeScale;         /*!< linetype scale, code 48 */
     bool visible;              /*!< entity visibility, code 60 */
     int color24;               /*!< 24-bit color, code 420 */
     std::string colorName;     /*!< color name, code 430 */
-    int space;                 /*!< space indicator 0 = model, 1 paper, code 67*/
+    DRW::Space space;          /*!< space indicator, code 67*/
     bool haveExtrusion;        /*!< set to true if the entity have extrusion*/
+    std::string image;         /*!< proxy entity graphics, code 92 and 310 */
+    std::list<DRW::Group*> groups; /*!< list of groups, code 102 */
+    DRW::ShadowMode shadow;    /*!< shadow mode, code 284 */
+    int material;              /*!< hard pointer id to material object, code 347 */
+    int plotStyle;             /*!< hard pointer id to plot style object, code 420 */
+    int transparency;          /*!< transparency, code 440 */
+    bool inGroup:1;            /*!< used to parse dxf groups */
+
 //***** dwg parse ********/
     duint8 nextLinkers; //aka nolinks //B
     duint8 plotFlags; //presence of plot style //BB
@@ -180,6 +186,8 @@ public:
     DRW_Coord basePoint;      /*!<  base point, code 10, 20 & 30 */
     double thickness;         /*!< thickness, code 39 */
     DRW_Coord extPoint;       /*!<  Dir extrusion normal vector, code 210, 220 & 230 */
+    // TNick: we're not handling code 50 - Angle of the X axis for
+    // the UCS in effect when the point was drawn
 };
 
 //! Class to handle line entity