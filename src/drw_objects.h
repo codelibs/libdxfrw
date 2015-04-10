3376b968ccebd4f1a0371c015c26a9502b685765
Rallaz <rallazz@gmail.com>
2015-04-10 16:45:18 +0200

    added copy ctor to prevent double free

--- src/drw_objects.h
+++ src/drw_objects.h
@@ -71,6 +71,17 @@ public:
         extData.clear();
     }
 
+    DRW_TableEntry(const DRW_TableEntry& e) {
+        tType = e.tType;
+        flags = e.flags;
+        numReactors = e.numReactors;
+        xDictFlag = e.xDictFlag;
+        parentHandle = e.parentHandle;
+        curr = e.curr;
+        for (std::vector<DRW_Variant*>::const_iterator it=e.extData.begin(); it!=e.extData.end(); ++it){
+            extData.push_back(new DRW_Variant(*(*it)));
+        }
+    }
 
 protected:
     void parseCode(int code, dxfReader *reader);
